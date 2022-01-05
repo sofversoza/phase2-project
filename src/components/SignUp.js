c71b757f945eef100c4a3838989145f126404fc1
maxcharles500 <93228208+maxcharles500@users.noreply.github.com>
2022-01-05 08:25:54 -0800

    base app

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,2 +1,30 @@
 // user inputs info to create an account 
 //once complete routes to Home page
+import React from 'react'
+import {useNavigate} from 'react-router-dom';
+
+function SignUp() {
+    let navigate = useNavigate();
+
+    const handleSignUp = () => {
+        navigate('/homepage')
+    }
+    return (
+        <div>
+            <header>Sign Up Form</header>
+            <input className='sugn-up-form' placeholder='Username'></input>
+            <br/>
+            <input className='sugn-up-form' placeholder='First Name'></input>
+            <br/>
+            <input className='sugn-up-form' placeholder='Last Name'></input>
+            <br/>
+            <input className='sugn-up-form' placeholder='Email'></input>
+            <br/>
+            <input className='sugn-up-form' placeholder='Password'></input>
+            <br/>
+            <button className='sugn-up-form' type='submit' onClick={handleSignUp}>Submit</button>
+        </div>
+    )
+}
+
+export default SignUp
