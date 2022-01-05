e952e5e752951cc0fb08198190a70522d5cd55d9
maxcharles500 <93228208+maxcharles500@users.noreply.github.com>
2022-01-05 08:21:46 -0800

    base app

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,22 @@
 // render two buttons login form(renders loginform on click) and sign up form(renders signup form on click)
-import React from 'react'
-
+// render two buttons login form(renders loginform on click) and sign up form(renders signup form on click)
+import React from 'react';
+import {NavLink} from 'react-router-dom';
 function LandingPage() {
+
+  
 
     return (
         <div>
-            <button>
+            <NavLink to='/login' style={{color: "white", textDecoration: 'none', fontSize: '3rem'}}>
                 Log In
-            </button>
-            <button>
+            </NavLink>
+            <br/>
+            <NavLink to='/sign-up' style={{color: "white", textDecoration: 'none', fontSize: '3rem'}} >
                 Sign Up
-            </button>
+            </NavLink>
         </div>
     )
 }
 
 export default LandingPage
-
