d3a16aa7cc753b464c8d79fd3d138c1eb8a947df
maxcharles500 <93228208+maxcharles500@users.noreply.github.com>
2022-01-05 08:22:38 -0800

    base app

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,2 +1,25 @@
 // user inputs info to login 
 //once complete routes to Home page
+import React from 'react'
+import {useNavigate} from 'react-router-dom';
+
+function LoginForm() {
+    let navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/homepage');
+    }
+  
+    return (
+        <div>
+            <header>Log In Form</header>
+            <input className='login-form' placeholder='Username'></input>
+            <br/>
+            <input className='login-form' placeholder='Password'></input>
+            <br/>
+            <button className='login-form' type='submit' onClick={handleLogin}>Submit</button>
+        </div>
+    )
+}
+
+export default LoginForm
