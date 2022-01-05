57d028b7692e3bce202f81868db83ff99c736925
sofiaversoza <sofversoza@gmail.com>
2022-01-05 13:49:52 -0700

    logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import LandingPage from './components/LandingPage'
-import SignUp from './components/SignUp'
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
+import '../App.css';
+import LandingPage from './LandingPage'
+import SignUp from './SignUp'
+import LoginForm from './LoginForm'
+import Home from './Home'
 import { Route, Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 //rfce = create starter component
@@ -31,13 +31,6 @@ function App() {
       />
       </Routes>
       
-
-      
-     
-      
-     
-
-
        {/* <LandingPage />
        <SignUp />
        <LoginForm /> */}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,42 @@
+
 import {Navbar, Container, Nav, Form, FormControl, Button} from 'react-bootstrap';
 
 
 
 function NavBar() {
     return (   
-        
-    <Navbar bg="dark" variant="dark" fixed='top'>
-    <Container>
-   
-    <Navbar.Brand href="#home">Logo // Current Username</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Liked Songs</Nav.Link>
-      <Nav.Link href="#pricing">Playlists</Nav.Link>
-    </Nav>
-    <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-    </Container>
-  </Navbar>
-    )
+   <>       
+      <Navbar bg="black" variant="dark" fixed='top' className="navbar">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img 
+              src="Q.png"
+              width="200"
+              height="200"
+              className="logo"
+              alt=""
+            />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Songs</Nav.Link>
+            <Nav.Link href="#pricing">Playlists</Nav.Link>
+          </Nav>
+    
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+    </>
+  )
 }
 
 export default NavBar
+
